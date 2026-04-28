b5fdda6dbdc9b369f2b8c0d098781db01179abc5
Divya <divyanareshkumarpatel@gmail.com>
2026-04-28 12:20:25 +0530

    project

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,7 @@
+import { create } from "domain";
+import { CreateUserDto } from "./create-user.dto";
+import {PartialType} from "@nestjs/mapped-types";
+
+export class UpdateUserDto extends PartialType(CreateUserDto){
+    
+}
